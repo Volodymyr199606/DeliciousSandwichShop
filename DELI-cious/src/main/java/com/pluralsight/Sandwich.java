3103e9b0c165ca9dc64b822b8f431ea09f39e45a
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-05-25 17:59:21 -0700

    Created Sandwich class, added fields, addd constructor and getters and setters

--- DELI-cious/src/main/java/com/pluralsight/Sandwich.java
+++ DELI-cious/src/main/java/com/pluralsight/Sandwich.java
@@ -1,5 +1,59 @@
 package com.pluralsight;
 
-public class Sandwich
-{
+import java.util.List;
+
+public class Sandwich {
+    private String size;
+    private String bread;
+    private boolean toasted;
+    private List<Topping> regularToppings;
+    private List<Topping> premiumToppings;
+
+    public Sandwich(String size, String bread, boolean toasted, List<Topping> regularToppings, List<Topping> premiumToppings) {
+        this.size = size;
+        this.bread = bread;
+        this.toasted = toasted;
+        this.regularToppings = regularToppings;
+        this.premiumToppings = premiumToppings;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        this.toasted = toasted;
+    }
+
+    public List<Topping> getRegularToppings() {
+        return regularToppings;
+    }
+
+    public void setRegularToppings(List<Topping> regularToppings) {
+        this.regularToppings = regularToppings;
+    }
+
+    public List<Topping> getPremiumToppings() {
+        return premiumToppings;
+    }
+
+    public void setPremiumToppings(List<Topping> premiumToppings) {
+        this.premiumToppings = premiumToppings;
+    }
 }
