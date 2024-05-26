cb60216d303ca47e8b8c99688a118d53ac2b5fa3
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-05-25 18:05:25 -0700

    Created Topping class , fields, constructor, and getters and setters

--- DELI-cious/src/main/java/com/pluralsight/Topping.java
+++ DELI-cious/src/main/java/com/pluralsight/Topping.java
@@ -1,5 +1,37 @@
 package com.pluralsight;
 
-public class Topping
-{
+public class Topping {
+    private String name;
+    private boolean isPremium;
+    private double cost;
+
+    public Topping(String name, boolean isPremium, double cost) {
+        this.name = name;
+        this.isPremium = isPremium;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public void setPremium(boolean premium) {
+        isPremium = premium;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
