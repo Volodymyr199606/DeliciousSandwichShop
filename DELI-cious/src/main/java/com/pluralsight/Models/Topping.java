cba6fc9e77f46917197a347a021cf10025d86cae
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-05-28 23:04:48 -0700

    build loggic for Toppins class

--- DELI-cious/src/main/java/com/pluralsight/Models/Topping.java
+++ DELI-cious/src/main/java/com/pluralsight/Models/Topping.java
@@ -1,5 +1,45 @@
 package com.pluralsight.Models;
 
 public class Topping {
+    private String name;
+    private boolean isPremium;
+    private boolean isExtra;
 
+    public Topping(String name, boolean isPremium, boolean isExtra) {
+        this.name = name;
+        this.isPremium = isPremium;
+        this.isExtra = isExtra;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
+
+    public double getCost(int size) {
+        double cost = 0.0;
+        if (isPremium) {
+            switch (size) {
+                case 4:
+                    cost = isExtra ? 1.50 : 1.00;
+                    break;
+                case 8:
+                    cost = isExtra ? 3.00 : 2.00;
+                    break;
+                case 12:
+                    cost = isExtra ? 4.50 : 3.00;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid size");
+            }
+        }
+        return cost;
+    }
 }
